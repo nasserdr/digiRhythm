4c80470f8ba09a84938209d792742a30108d267c
Dr Hassan Nasser <hassan.nasser@me.com>
2021-10-29 21:09:50 +0200

    Updated is_dgm_friendly so that it notifies when the data is null or when it contains less than 7 days.

--- R/is_dgm_friendly.R
+++ R/is_dgm_friendly.R
@@ -21,28 +21,47 @@ is_dgm_friendly <- function(data, verbose = FALSE){
 
   is_dgm <- TRUE
 
-  if("POSIXct" %in%  class(data[,1])  | "POSIXt" %in%  class(data[,1])){
-    message <- paste0(green('v Correct time format: '), 'First column has a Posixct Format')
-    print_v(message, verbose)
-  }else {
-    message <- paste0(red('x Inorrect time format: '), 'First column does not a Posixct Format')
+  if(is.null(data)){
+    message <- red('Data cannot be NULL')
     print_v(message, verbose)
     is_dgm <- FALSE
-  }
+  } else{
+    #Checking if the first column meets the requirements
+    if("POSIXct" %in%  class(data[,1])  | "POSIXt" %in%  class(data[,1])){
+      message <- paste0(green('v Correct time format: '), 'First column has a POSIXct Format')
+      print_v(message, verbose)
+    }else {
+      message <- paste0(red('x Inorrect time format: '), 'First column does not a POSIXct Format')
+      print_v(message, verbose)
+      is_dgm <- FALSE
+    }
 
 
-  if(ncol(data) == 1){
-    message <- paste0(red('x Illogical number of columns: '), 'The dataset has only one column. Minimum number of columns is 2')
-    print_v(message, verbose)
-    is_dgm <- FALSE
-  }else{
-    for(i in 2:ncol(data)){
-      if("numeric" %in% class(data[,i]) | "integer" %in%  class(data[,i])){
-        message <- green(paste('v Correct numeric format - Column', i, '==>', colnames(data)[i]))
-        print_v(message, verbose)
-      } else {
-        message <- red(paste('x Inorrect numeric format - Column', i, '==>', colnames(data)[i]))
-        print_v(message, verbose)
+    #Checking if data contains at least 7 days of data
+    if(length(unique(as.Date(data[,1]))) >= 7){
+      message <- paste0(green('v Number of days: '), 'Bigger or equal to 7')
+      print_v(message, verbose)
+    }else {
+      message <- paste0(red('x Number of days: '), 'Less than 7 (can\'t run the DFC algorithm later on')
+      print_v(message, verbose)
+      is_dgm <- FALSE
+    }
+
+    #Checking if we have more than one column
+    if(ncol(data) == 1){
+      message <- paste0(red('x Illogical number of columns: '), 'The dataset has only one column. Minimum number of columns is 2')
+      print_v(message, verbose)
+      is_dgm <- FALSE
+    }else{
+      #Checking if data are numeric
+      for(i in 2:ncol(data)){
+        if("numeric" %in% class(data[,i]) | "integer" %in%  class(data[,i])){
+          message <- green(paste('v Correct numeric format - Column', i, '==>', colnames(data)[i]))
+          print_v(message, verbose)
+        } else {
+          message <- red(paste('x Inorrect numeric format - Column', i, '==>', colnames(data)[i]))
+          print_v(message, verbose)
+        }
       }
     }
   }
