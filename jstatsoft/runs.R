dc738c4b1fdf007c2d87788722f6a02f8c131e77
Nasser Roland <roland.nasser@agroscope.admin.ch>
2023-04-28 10:52:43 +0200

    Added version Marianne

--- jstatsoft/runs.R
+++ jstatsoft/runs.R
@@ -5,5 +5,8 @@ library("tools")
 #install_github('https://github.com/nasserdr/digiRhythm')
 
 setwd("~/projects/digiRhythm/jstatsoft")
-Sweave('article.Rnw')
-texi2pdf("article.tex")
+Sweave('article_comr.Rnw')
+texi2pdf("article_comr.tex")
+
+# To rerun the pictures:
+# Check: ~/projects/digiRhythm/examples/code_snipets_statsoft_paper.R
