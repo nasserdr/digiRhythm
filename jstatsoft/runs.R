31dd2e43e18b6f75e0a2d1eb39838d04a26ae9bc
Nasser Roland <roland.nasser@agroscope.admin.ch>
2023-05-12 16:37:07 +0200

    Fixed the conflict but files got merged.

--- jstatsoft/runs.R
+++ jstatsoft/runs.R
@@ -1,12 +1,12 @@
 #For runs
-library(devtools)
+library("devtools")
 library("tools")
 
 #install_github('https://github.com/nasserdr/digiRhythm')
 
 setwd("~/projects/digiRhythm/jstatsoft")
-Sweave('article_comr.Rnw')
-texi2pdf("article_comr.tex")
+Sweave('article.Rnw')
+texi2pdf("article.tex")
 
 # To rerun the pictures:
 # Check: ~/projects/digiRhythm/examples/code_snipets_statsoft_paper.R
