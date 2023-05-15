ab79250d49932e30d902531c23430d7008855abf
Nasser Roland <roland.nasser@agroscope.admin.ch>
2023-05-15 14:07:42 +0200

    removed non-used personal versions

--- jstatsoft/runs.R
+++ jstatsoft/runs.R
@@ -3,7 +3,6 @@ library("devtools")
 library("tools")
 
 #install_github('https://github.com/nasserdr/digiRhythm')
-
 setwd("~/projects/digiRhythm/jstatsoft")
 Sweave('article.Rnw')
 texi2pdf("article.tex")
