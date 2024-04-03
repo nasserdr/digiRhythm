4619c8381910928ba705485b1ce54936bc1451d6
Nasser Roland <roland.nasser@agroscope.admin.ch>
2024-04-03 11:44:20 +0200

    Deleted an unneeded file

--- R/run.R
+++
@@ -1,8 +0,0 @@
-library("devtools")
-library("tools")
-
-#tinytex::tlmgr_install("textcase")
-
-setwd("~/projects/digiRhythm/sagepub/original/")
-#install_github('https://github.com/nasserdr/digiRhythm')
-texi2pdf("Sage_LaTeX_Guidelines.tex")
