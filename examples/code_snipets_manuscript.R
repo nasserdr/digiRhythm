c63ca3787dab7b174e8c9017055c47dc999fb6d0
Nasser Roland <roland.nasser@agroscope.admin.ch>
2024-04-05 10:42:22 +0200

    Fixed a glitch in the vignettes. Finalized the code for the paper.

--- examples/code_snipets_manuscript.R
+++ examples/code_snipets_manuscript.R
@@ -1,21 +1,32 @@
+#' General info
+#' This code is to reproduce the results in the digiRhythm paper.
+#' The code was test with R 4.3.3
+#' For the below imported libraries, if you get an error, please install the library using the command install.packages("library_name")
+#' Before you start, click on session ->  set working directory -> to source file location
 library(ggplot2)
 library(magrittr)
 library(lubridate)
 library(dplyr)
-library(digiRhythm)
 library(lomb)
 library(patchwork)
-
 
 ###############################################################################
 ####################### Installing DigiRhythm ##################################
 
 # Install the devtools package if you haven't already
+
+# To install the development version from GitHub
 #install.packages("devtools")
-devtools::install_github("nasserdr/digiRhythm", dependencies = TRUE)
+#devtools::install_github("nasserdr/digiRhythm", dependencies = TRUE)
+
+# To install the stable version from CRAN
+install.packages('digiRhythm')
+library(digiRhythm)
 
 ###############################################################################
-####################### FIGURE: 2 ###################################
+################################## FIGURE: 2 ##################################
+###############################################################################
+
 # LSP on purely synthetic data ----------------------------
 #Creating time grid
 sampling = 15*60 #15 minutes
@@ -56,7 +67,7 @@ for (period in c(24, 6)){
       axis.text = element_text(color = "#000000"),
       text = element_text(size = 15),
       panel.background = element_rect(fill = "white"),
-      axis.line = element_line(size = 0.5),
+      axis.line = element_line(linewidth = 0.5),
       legend.key = element_rect(fill = "white"),
       legend.key.width = unit(0.5, "cm"),
       legend.justification ="right",
@@ -64,9 +75,6 @@ for (period in c(24, 6)){
       legend.position = c(1,0.89),
       plot.margin = margin(t = 50)) +
       scale_x_continuous(n.breaks = 8)
-
-      # axis.text  = element_blank(),
-      # axis.ticks  = element_blank())
 
   name <- paste0('figures/sig', period, '.pdf')
   ggsave(
@@ -80,7 +88,7 @@ for (period in c(24, 6)){
     limitsize = TRUE)
 
   df$num <- NULL
-  lsp_plot <- lomb_scargle_periodogram(df, alpha = 0.01, sampling = 15, plot = TRUE, extra_info_plot = FALSE )
+  lsp_plot <- lomb_scargle_periodogram(df, alpha = 0.01, sampling = 15, plot = FALSE, extra_info_plot = FALSE )
 
   lsp_df <- lsp_plot$lsp_data
   ggplot(data = lsp_df, aes(x = frequency_hz, y = power)) +
@@ -92,7 +100,7 @@ for (period in c(24, 6)){
       panel.background = element_rect(fill = "white"),
       axis.text = element_text(color = "#000000"),
       text = element_text(size = 15),
-      axis.line = element_line(size = 0.5),
+      axis.line = element_line(linewidth = 0.5),
       legend.key = element_rect(fill = "white"),
       legend.key.width = unit(0.5, "cm"),
       legend.justification ="right",
@@ -106,7 +114,7 @@ for (period in c(24, 6)){
   name <- paste0('figures/lsp', period, '.pdf')
   ggsave(
     name,
-    plot = lsp <- last_plot(),
+    plot = lsp <- last_plot() ,
     device = 'pdf',
     width = w,
     height = h,
@@ -122,7 +130,7 @@ for (period in c(24, 6)){
 
 
 wrap_plots(all_plots, ncol = 2)
-#Creating a signal with a period = 12 and computing its lsp
+# Creating a signal with a period = 12 and computing its lsp
 
 
 
@@ -152,7 +160,7 @@ ggplot(data = df, aes(x = num, y = activity)) +
     axis.text = element_text(color = "#000000"),
     text = element_text(size = 15),
     panel.background = element_rect(fill = "white"),
-    axis.line = element_line(size = 0.5),
+    axis.line = element_line(linewidth = 0.5),
     legend.key = element_rect(fill = "white"),
     legend.key.width = unit(0.5, "cm"),
     legend.justification ="right",
@@ -172,7 +180,7 @@ ggsave(
   dpi = dp,
   limitsize = TRUE)
 df$num <- NULL
-lsp_plot <- lomb_scargle_periodogram(df, alpha = 0.01, sampling = 15, plot = TRUE, extra_info_plot = FALSE)
+lsp_plot <- lomb_scargle_periodogram(df, alpha = 0.01, sampling = 15, plot = FALSE, extra_info_plot = FALSE)
 
 lsp_df <- lsp_plot$lsp_data
 ggplot(data = lsp_df, aes(x = frequency_hz, y = power)) +
@@ -184,7 +192,7 @@ ggplot(data = lsp_df, aes(x = frequency_hz, y = power)) +
     panel.background = element_rect(fill = "white"),
     axis.text = element_text(color = "#000000"),
     text = element_text(size = 15),
-    axis.line = element_line(size = 0.5),
+    axis.line = element_line(linewidth = 0.5),
     legend.key = element_rect(fill = "white"),
     legend.key.width = unit(0.5, "cm"),
     legend.justification ="right",
@@ -209,7 +217,7 @@ all_plots[[6]] <- lsp
 
 wrap_plots(all_plots, ncol = 2)
 
-name <- paste0('figures/Figure 1.png')
+name <- paste0('figures/Figure 2.png')
 
 ggsave(
   name,
@@ -222,12 +230,13 @@ ggsave(
   limitsize = TRUE)
 
 ###############################################################################
-####################### FIGURE: alternative for real_lsp#######################
+################################## FIGURE: 3 ##################################
+###############################################################################
 
 #Real data
 
-data("df691b_1", package = 'digiRhythm')
-df <- df691b_1
+data("df516b_2", package = 'digiRhythm')
+df <- df516b_2
 df <- df[1:672,c(1,2)]
 
 ggplot(data = df, aes(x = datetime, y = Motion.Index)) +
@@ -239,7 +248,7 @@ ggplot(data = df, aes(x = datetime, y = Motion.Index)) +
     axis.text = element_text(color = "#000000"),
     text = element_text(size = 15),
     panel.background = element_rect(fill = "white"),
-    axis.line = element_line(size = 0.5),
+    axis.line = element_line(linewidth = 0.5),
     legend.key = element_rect(fill = "white"),
     legend.key.width = unit(1, "cm"),
     legend.justification ="right",
@@ -247,71 +256,151 @@ ggplot(data = df, aes(x = datetime, y = Motion.Index)) +
     legend.position = c(1,0.89),
     plot.margin = margin(t = 50))
 
-name <- paste0('figures/real_sig.pdf')
+name <- paste0('figures/Figure 3.png')
 
 ggsave(
   name,
   plot = signal <- last_plot(),
-  device = 'pdf',
+  device = 'png',
   width = w,
   height = h,
   scale = 1,
   dpi = dp,
   limitsize = TRUE)
 
-lomb_scargle_periodogram(df, alpha = 0.01, sampling = 15, plot = TRUE, extra_info_plot = FALSE)
+###############################################################################
+################################## FIGURE: 4 ##################################
+###############################################################################
 
-name <- paste0('figures/real_lsp.pdf')
+lomb_scargle_periodogram(df, alpha = 0.01, sampling = 15, plot = TRUE, extra_info_plot = TRUE)
+
+name <- paste0('figures/Figure 4.png')
 ggsave(
   name,
   plot = lsp <- last_plot(),
-  device = 'pdf',
+  device = 'png',
   width = w,
   height = h,
   scale = 1,
   dpi = dp,
   limitsize = TRUE)
 
-signal | lsp
-name <- paste0('figures/real_lsp_signal.pdf')
+###############################################################################
+################################## FIGURE: 5 ##################################
+###############################################################################
+df <- df516b_2 # considering the whole dataset
+df <- resample_dgm(df, 15)
+activity = names(df)[2] # considering the first activity variable (second column usually)
+start = "2020-05-01"
+end = "2020-06-15"
+my_actogram <- actogram(df, activity, activity_alias = 'Motion Index' , start, end, save = NULL)
+
+name <- paste0('figures/Figure 5.png')
 ggsave(
   name,
-  plot = last_plot(),
-  device = 'pdf',
-  width = 10,
-  height = 3,
-  scale = 1.5,
-  dpi = 500,
+  plot = lsp <- last_plot(),
+  device = 'png',
+  width = w,
+  height = h,
+  scale = 1,
+  dpi = dp,
   limitsize = TRUE)
+
+
+###############################################################################
+################################## FIGURE: 6 ##################################
+###############################################################################
+my_daa <- daily_average_activity(df,
+                                 activity,
+                                 activity_alias = 'Motion Index' ,
+                                 start,
+                                 end,
+                                 save = NULL)
+name <- paste0('figures/Figure 6.png')
+ggsave(
+  name,
+  plot = lsp <- last_plot(),
+  device = 'png',
+  width = w,
+  height = h,
+  scale = 1,
+  dpi = dp,
+  limitsize = TRUE)
+
+###############################################################################
+################################## FIGURE: 7 ##################################
+###############################################################################
+day_time = c("06:30:00", "16:30:00")
+night_time = c("18:00:00", "T05:00:00")
+my_di <- diurnality(df, activity, day_time, night_time, save = NULL)
+name <- paste0('figures/Figure 7.png')
+ggsave(
+  name,
+  plot = lsp <- last_plot(),
+  device = 'png',
+  width = w,
+  height = h,
+  scale = 1,
+  dpi = dp,
+  limitsize = TRUE)
+
+###############################################################################
+################################## FIGURE: 8 ##################################
+###############################################################################
+data("timedata", package = 'digiRhythm') # Loading another dataset where there is a daylight shift
+td <- as.data.frame(timedata)
+my_sliding_di <- sliding_DI(df,
+                            activity,
+                            td)
+name <- paste0('figures/Figure 8.png')
+ggsave(
+  name,
+  plot = lsp <- last_plot(),
+  device = 'png',
+  width = w,
+  height = h,
+  scale = 1,
+  dpi = dp,
+  limitsize = TRUE)
+
+###############################################################################
+################################## FIGURE: 9 ##################################
+###############################################################################
 
 #Verifying the datasets
 data("df516b_2", package = 'digiRhythm')
 df <- df516b_2
 df <- resample_dgm(df, 15)
-activity = names(df)[3]
+activity = names(df)[2]
 
-my_dfc <- dfc(df, activity = activity,  sig = 0.05, plot = TRUE, verbose = FALSE)
+my_dfc <- dfc(df, activity = activity,  sig = 0.05, plot = FALSE, verbose = FALSE)
+name <- paste0('figures/Figure 9.png')
+ggsave(
+  name,
+  plot = last_plot(),
+  device = 'png',
+  width = w,
+  height = h,
+  scale = 1,
+  dpi = dp,
+  limitsize = TRUE)
+
+###############################################################################
+################################## FIGURE: 10 ##################################
+###############################################################################
 
 my_dfc +
   theme(
     text=element_text(family="Times", size=8),
     axis.text.y=element_text(size=15, colour="red"))
 
-
-data <- df[1:672,c(1,2)]
-
-ggplot(data = data, aes(x = datetime, y = Motion.Index)) +
-  geom_line() +
-  xlab('Date') +
-  ylab('Motion Index') +
-  ggtitle('') +
-  theme(
-    panel.background = element_rect(fill = "white"),
-    axis.line = element_line(size = 0.5),
-    legend.key = element_rect(fill = "white"),
-    legend.key.width = unit(1, "cm"),
-    legend.justification ="right",
-    legend.key.size = unit(7, "pt"),
-    legend.position = c(1,0.89),
-    plot.margin = margin(t = 50))
-
+name <- paste0('figures/Figure 10.png')
+ggsave(
+  name,
+  plot = last_plot(),
+  device = 'png',
+  width = w,
+  height = h,
+  scale = 1,
+  dpi = dp,
+  limitsize = TRUE)
